e49cb6bdc71d40d1cca4a2646adbeaeb8838c7cb
Christopher O'Brien <christopherhenryobrien@gmail.com>
2017-09-18 06:52:20 -0700

    pushing completed test - all requirements complete

--- sampleapp_be/views.py
+++ sampleapp_be/views.py
@@ -1,5 +1,7 @@
 from pyramid.response import Response
 from pyramid.view import view_config
+import json
+import csv
 
 
 # /
@@ -25,5 +27,82 @@ def get_data(request):
         ('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
     )
     request.response.headerlist.extend(headers)
-    return {}
-
+    
+    propertyData = []
+    key_column_index_dict = {'PROP_NAME': None, 'ADDRESS': None, 'CITY': None, 'STATE_ID': None, 'ZIP': None}
+    
+    with open('sampleapp_be/assets/properties.csv') as file:
+        data = csv.reader(file)
+        is_first_row = True 
+        for row in data:
+            #record column index for each required key
+            if is_first_row:
+                index = 0
+                for k in row:
+                    if k == 'PROP_NAME':
+                        key_column_index_dict['PROP_NAME'] = index
+                    elif k == 'ADDRESS':
+                        key_column_index_dict['ADDRESS'] = index
+                    elif k == 'CITY':
+                        key_column_index_dict['CITY'] = index
+                    elif k == 'STATE_ID':
+                        key_column_index_dict['STATE_ID'] = index
+                    elif k == 'ZIP':
+                        key_column_index_dict['ZIP'] = index
+                    else:
+                        pass   
+                    index += 1
+                is_first_row = False
+            #construct json object containing required fields
+            else:
+                if row[key_column_index_dict['STATE_ID']].lower() == 'ca':
+                    
+                    #calculate values for MISSING_FIELD_COUNT & MISSING_DATA_ENCODING keys
+                    missing_field_count = 0
+                    missing_data_encoding = None #possible issue: when complete this variable's value will be contingent on the order the attributes in the properties.csv file; however, the json object might not reflect the file's order as json doesn't guarantee order of attributes
+                    missing_data_encoding_list = [] #holds value existence streak to be concatenated into 1 integer once all pairs examined
+                    is_first_pair = True
+                    key_has_no_value = False #reflects the existence state of the pair value in the previous pair
+                    value_existence_streak = 0 
+                    for key in key_column_index_dict:
+                        if is_first_pair:
+                            if row[key_column_index_dict[key]] == '':
+                                missing_field_count += 1
+                                key_has_no_value = True
+                            value_existence_streak += 1
+                            is_first_pair = False
+                        else:
+                            if row[key_column_index_dict[key]] == '':
+                                missing_field_count += 1
+                                if key_has_no_value == True:
+                                    value_existence_streak += 1
+                                else:
+                                    key_has_no_value = True
+                                    missing_data_encoding_list.append(value_existence_streak)
+                                    value_existence_streak = 1
+                            else:
+                                if key_has_no_value == True:
+                                    key_has_no_value = False
+                                    missing_data_encoding_list.append(value_existence_streak)
+                                    value_existence_streak = 1
+                                else:
+                                    value_existence_streak += 1
+                    missing_data_encoding_list.append(value_existence_streak)
+                    
+                    #concatenate value existence streaks into 1 integer
+                    if(value_existence_streak == len(key_column_index_dict)):
+                        missing_data_encoding = value_existence_streak
+                    else:
+                        is_first_streak = True
+                        for streak in missing_data_encoding_list:
+                            if is_first_streak:
+                                missing_data_encoding = streak
+                                is_first_streak = False
+                            else:
+                                missing_data_encoding = int(str(missing_data_encoding) + str(streak))
+    
+                    record = {"PROP_NAME": row[key_column_index_dict['PROP_NAME']], "ADDRESS": row[key_column_index_dict['ADDRESS']], "CITY": row[key_column_index_dict['CITY']], "STATE_ID": row[key_column_index_dict['STATE_ID']], "ZIP": row[key_column_index_dict['ZIP']], "MISSING_FIELD_COUNT": missing_field_count, "MISSING_DATA_ENCODING": missing_data_encoding}
+                    propertyData.append(record)
+                    
+    json.dumps(propertyData) #serialize propertyData to JSON format
+    return propertyData
--- sampleapp_fe/client/App.js
+++ sampleapp_fe/client/App.js
@@ -1,13 +1,229 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello World!</h1>
-      </div>
-    )
-  }
+    constructor() {
+        super();
+        this.state = {propertyData: []};
+        this.fetchData = this.fetchData.bind(this);
+    }
+    fetchData() {
+        fetch('/data', {
+            mode: 'no-cors',
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(response => response.json())
+        .then(propertyData => this.setState({propertyData: propertyData}));
+    }
+    componentWillMount() {
+        document.body.style.backgroundColor = "#FEFCFF";
+        document.body.style.height = "100%";
+    }
+    render() {
+        const style = {
+            container: {
+                margin:"0 auto",
+                width: "90%",
+                minHeight: "100%",
+                position: "relative",
+                fontFamily: "'Raleway', sans-serif",
+                color: "#3D3C3A"
+            },
+            header: {
+                margin:"0 auto",
+                width: "90%",
+                height: "100px",
+                position: "relative",
+                top: "0",
+                left: "0"
+            },
+            title: {
+                fontSize: "70px",
+                verticalAlign: "bottom"
+            },
+            bodyContainer: {
+                margin:"0 auto",
+                width: "90%",
+                position: "relative"
+            },
+            button: {
+                verticalAlign: "middle",
+                float: "left"
+            }
+        };
+        return (
+            <div style={style.container}>
+                <div style={style.header}>
+                    <h1 style={style.title}>Properties in California</h1>
+                </div>
+                <div style={style.bodyContainer}>
+                    <div style={style.button}>
+                        <Button_Component onClick={this.fetchData} />
+                    </div>
+                    <div style={{clear:"both"}}></div>
+                    <div>
+                        <Table_Component propertyData={this.state.propertyData} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+class Button_Component extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const style = {
+            backgroundColor: "#314CB6",
+            border: "0px",
+            color: "#F7F7FF",
+            borderRadius: "10px",
+            MozBorderRadius: "10px",
+            WebkitBorderRadius: "10px",
+            height: "50px",
+            width: "200px",
+            fontSize: "20px",
+            textAlign: "center"
+        };
+        return (
+            <button style={style} onClick={this.props.onClick}>List Properties</button>
+        )
+    }
+}
+
+class Table_Component extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {  
+        const style = {
+            table: {
+                borderSpacing: "0px 5px",
+                textAlign: "center",
+            },
+            thead: {
+            },
+            thRow: {
+                height:"80px", 
+                margin: "10px 0 5px 0",
+                backgroundColor:"#F2F2F3"
+            },
+            tbody: {
+            },
+            nameHeader: {
+                borderTopLeftRadius: "15px",
+                MozBorderRadiusTopLeft: "15px",
+                WebkitBorderTopLeftRadius: "15px",
+                
+                borderBottomLeftRadius: "15px",
+                MozBorderRadiusBottomLeft: "15px",
+                WebkitBorderBottomLeftRadius: "15px",
+            },
+            missingDataEncodingHeader: {
+                borderTopRightRadius: "15px",
+                MozBorderRadiusTopRight: "15px",
+                WebkitBorderTopRightRadius: "15px",
+                
+                borderBottomRightRadius: "15px",
+                MozBorderRadiusBottomRight: "15px",
+                WebkitBorderBottomRightRadius: "15px",
+            }
+        };
+        
+        var tableRows = this.props.propertyData.map(function(rowData, index){
+            return <TR_Component key={index} rowData={rowData} />
+        });
+        
+        var tableSkeleton =
+            <table style={style.table}>
+                <thead style={style.thead}>
+                    <tr style={style.thRow}>
+                        <th style={style.nameHeader}>PROP_NAME</th>
+                        <th>ADDRESS</th>
+                        <th>CITY</th>
+                        <th>STATE_ID</th>
+                        <th>ZIP</th>
+                        <th>MISSING_ <br /> FIELD_ <br /> COUNT</th>
+                        <th style={style.missingDataEncodingHeader}>MISSING_ <br /> DATA_ <br /> ENCODING</th>
+                    </tr>
+                </thead>
+                <tbody style={style.tbody}>
+                    {tableRows}
+                </tbody>
+            </table>
+        ;
+        
+        var element = (tableRows.length > 0) ? tableSkeleton : null;
+        
+        return (
+            element
+        )
+    }
+}
+
+class TR_Component extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const style = {
+            row: {
+                backgroundColor: "#F4F7FF",
+                height: "40px",
+                fontSize: "16px",
+                marginBottom: "3px"
+            },
+            nameCell: {
+                borderTopLeftRadius: "15px",
+                MozBorderRadiusTopLeft: "15px",
+                WebkitBorderTopLeftRadius: "15px",
+                
+                borderBottomLeftRadius: "15px",
+                MozBorderRadiusBottomLeft: "15px",
+                WebkitBorderBottomLeftRadius: "15px",
+            },
+            missingDataEncodingCell: {
+                borderTopRightRadius: "15px",
+                MozBorderRadiusTopRight: "15px",
+                WebkitBorderTopRightRadius: "15px",
+                
+                borderBottomRightRadius: "15px",
+                MozBorderRadiusBottomRight: "15px",
+                WebkitBorderBottomRightRadius: "15px",
+            }
+            
+        };
+        
+        var property = this.props.rowData;
+        return (
+            <tr style={style.row}>
+                <TD_Component value={property.PROP_NAME} style={style.nameCell} />
+                <TD_Component value={property.ADDRESS} />
+                <TD_Component value={property.CITY} />
+                <TD_Component value={property.STATE_ID} />
+                <TD_Component value={property.ZIP} />
+                <TD_Component value={property.MISSING_FIELD_COUNT} />
+                <TD_Component value={property.MISSING_DATA_ENCODING} style={style.missingDataEncodingCell} />
+            </tr>
+        )
+    }
+}
+
+class TD_Component extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <td style={this.props.style}>
+                {this.props.value}
+            </td>
+        )
+    }
 }
 
 export default App;
